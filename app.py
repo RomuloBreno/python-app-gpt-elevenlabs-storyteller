0c527315ea0b56f6fa49239e450d307f79772fd8
_b.romulo <68164627+RomuloBreno@users.noreply.github.com>
2024-09-28 21:08:17 -0300

    Connection Heroku

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+from flask import Flask, jsonify
 from flask import Flask, jsonify, request
-import app.submit_action as submit
+import aplication.submit_action as submit
 import config
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello, World!"})
 
 config_ = config.load_config()
 dir_save = config_['appSettings']['dir']['save']
@@ -17,4 +25,4 @@ def data_handler():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
